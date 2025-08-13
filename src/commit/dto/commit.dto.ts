c6d984439bbf6d6fbadc6e671e9c7be54413d64d
Khanh2108 <nguyenkhanhduyenhai@gmail.com>
2025-08-13 09:38:42 +0700

    update swagger API

--- src/commit/dto/commit.dto.ts
+++ src/commit/dto/commit.dto.ts
@@ -2,28 +2,43 @@ import { IsString, IsNotEmpty, IsUrl, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CommitDto {
-  @ApiProperty({ description: 'URL of the target Git repository' })
+  @ApiProperty({
+    description: 'URL of the target Git repository',
+    example: 'https://github.com/username/repo.git' // Dummy data
+  })
   @IsString()
   @IsNotEmpty()
   @IsUrl()
   repoUrl: string;
 
-  @ApiProperty({ description: 'Branch of the target Git repository' })
+  @ApiProperty({
+    description: 'Branch of the target Git repository',
+    example: 'main'
+  })
   @IsString()
   @IsNotEmpty()
   repoBranch: string;
 
-  @ApiProperty({ description: 'GitHub Personal Access Token' })
+  @ApiProperty({
+    description: 'GitHub Personal Access Token',
+    example: 'your_github_token_here'
+  })
   @IsString()
   @IsNotEmpty()
   githubToken: string;
 
-  @ApiProperty({ description: 'GitHub user name for commits' })
+  @ApiProperty({
+    description: 'GitHub user name for commits',
+    example: 'your_github_username_here'
+  })
   @IsString()
   @IsNotEmpty()
   githubName: string;
 
-  @ApiProperty({ description: 'GitHub user email for commits' })
+  @ApiProperty({
+    description: 'GitHub user email for commits',
+    example: 'your_github_email_here'
+  })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
